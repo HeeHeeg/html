02a210263e29788b3586e384728d5077a6595d4a
HeeHeeg <qmffndoden@naver.com>
2023-06-15 12:05:18 +0900

    user post success

--- src/main/java/com/playdata/todos/servlet/UserServlet.java
+++ src/main/java/com/playdata/todos/servlet/UserServlet.java
@@ -1,5 +1,8 @@
 package com.playdata.todos.servlet;
 
+import com.playdata.todos.dao.UserDao;
+import com.playdata.todos.dto.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -19,11 +22,15 @@ public class UserServlet extends HttpServlet {
         String username = req.getParameter("username");
         String password = req.getParameter("password");
         String name = req.getParameter("name");
-        System.out.println(username);
-        System.out.println(password);
-        System.out.println(name);
-        resp.setStatus(201);
+//        System.out.println(username);
+//        System.out.println(password);
+//        System.out.println(name);
+        User user = new User(null, username, password, name, null);
+        UserDao userDao = new UserDao();
+        userDao.insert(user);
+
         resp.sendRedirect("/user"); // 추가 후 재실행 해봄  이 서버로 보내버리는것.? 그럼 다시 doget으로 가서 페이지를 다시 보여주는 것.
+        resp.setStatus(201);
 
     }
 }
